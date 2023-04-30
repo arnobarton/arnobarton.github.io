d48f827422a40098d8798212a260889c859be4a8
Arno Barton <abarto24@igs.vic.edu.au>
2023-04-30 21:27:58 +1000

    BIG commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function showForm() {
+    var form = document.getElementById("form");
+    form.style.display = "block";
+  }
+// Get the button element
+const button = document.querySelector('#about');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+  // Code to execute when the button is clicked
+});
+  
